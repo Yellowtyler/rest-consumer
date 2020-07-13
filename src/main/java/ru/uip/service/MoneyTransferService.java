8b992d8a735e20c25ae56ef8e68f91538bd0f511
Daniil <davydovdanya18@gmail.com>
2020-07-13 17:09:12 +0300

    added api.yml

--- src/main/java/ru/uip/service/MoneyTransferService.java
+++ src/main/java/ru/uip/service/MoneyTransferService.java
@@ -2,7 +2,6 @@ package ru.uip.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import ru.uip.model.EnumAccountStatus;
 import ru.uip.model.JsonAccount;
 import ru.uip.model.MoneyTransfer;
 import ru.uip.model.MoneyTransferResult;
@@ -15,7 +14,7 @@ public class MoneyTransferService {
     public MoneyTransferResult transferMoney(MoneyTransfer moneyTransfer) {
         JsonAccount from = accountProxyService.findById(moneyTransfer.getFromAccountNumber()).getBody();
         JsonAccount to = accountProxyService.findById(moneyTransfer.getToAccountNumber()).getBody();
-        
+
         from.setAccountBalance(from.getAccountBalance() - moneyTransfer.getAmmount());
         to.setAccountBalance(to.getAccountBalance() + moneyTransfer.getAmmount());
 
